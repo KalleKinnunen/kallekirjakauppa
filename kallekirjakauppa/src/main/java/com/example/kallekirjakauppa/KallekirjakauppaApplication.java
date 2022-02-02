e39efc961d19d25dd6ac9ec4f1389918a58e5fab
KalleKinnunen <98499664+KalleKinnunen@users.noreply.github.com>
2022-02-02 13:35:29 +0200

    Add files via upload

---
+++ kallekirjakauppa/src/main/java/com/example/kallekirjakauppa/KallekirjakauppaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kallekirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KallekirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KallekirjakauppaApplication.class, args);
+	}
+
+}
